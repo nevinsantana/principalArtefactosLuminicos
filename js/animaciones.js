7e1984d206e186b41fb0151da442fada52ff5887
Nevin Santana <roll.93@hotmail.com>
2015-10-09 14:33:17 -0500

    Errores corregidos de Menú nuevo y menú viejo en interaccion con el slider

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,10 +1,8 @@
-var width = (document.body.clientWidth);
-
 function normaliza(){
   var width = (document.body.clientWidth);
   if(width>1048){
   document.getElementById("nav").style.top="13em";
-  document.getElementById("nav").style.zIndex="1";
+  document.getElementById("nav").style.zIndex="3";
   document.getElementById("nav").style.opacity="1";
   }
   else {
@@ -89,10 +87,14 @@ function normaliza(){
   }
 
   function apareceMenu() {
+    var width = document.body.clientWidth;
+    if(width<1048) {
     document.getElementById("nav").style.top="16.6em";
-    document.getElementById("nav").style.zIndex="1";
+    document.getElementById("nav").style.zIndex="3";
     document.getElementById("nav").style.opacity="0.95";
     document.getElementById("closeMenu").style.display="inline";
+    }
+    else{}
   }
 
   function desapareceMenu() {
