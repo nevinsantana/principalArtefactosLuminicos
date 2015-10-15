bd5b789c734f2248ca8a643414353607a7a9bab9
Nevin Santana <roll.93@hotmail.com>
2015-10-15 10:31:49 -0500

    BibliotecaIlu.php Marco gruñón

--- Productos.php
+++ Productos.php
@@ -66,7 +66,7 @@
 
 			<div id="modal2" class="modalmask">
 				<div class="modalbox rotate">
-					<a href="#close" title="Close" class="close">X</a>
+					<a href="#close" title="Cerrar" class="close">X</a>
 					<h2>2AV</h2>
 					<img src="images/Fluorescente/2AV.jpg" />
 					<P>Un producto excepcional iluminación general que se desempeña bien en grandes espacios con techos </br>
@@ -96,7 +96,7 @@
 
 			<div id="modal3" class="modalmask">
 				<div class="modalbox rotate">
-					<a href="#close" title="Close" class="close">X</a>
+					<a href="#close" title="Cerrar" class="close">X</a>
 					<h2>02454</h2>
 					<img src="images/Interior/02454.jpg" />
 					<P>Especial para tiendas minoristas, oficinas, escuelas, Lobbies y Restaurantes. El sistema de iluminación </br>
@@ -106,10 +106,7 @@
 						para contribuir con el estilo y la función de cualquier entorno de iluminación. La cubierta de cobre opcional</br>
 						 pone de manifiesto la belleza del metal clásico de añadir elegancia al diseño retro-industrial. La familia</br>
 						  de 02454 está disponible en  fluorescentes compactas muy popular, incandescente, aditivos metalicos y pulso </br>
-							para la flexibilidad máxima del diseño. La familia de productos 02454 está disponible con Elite Holophane de</br>
-							 la serie de balastros electrónicos HID, obteniendo una eficiencia excepcional en términos de ahorro de energía.</br>
-							  El matrimonio del control visualmente atractiva óptica precisa, eficiente rendimiento de la opción del</br>
-								 balastro electrónico con un rendimiento superior.</br></p>
+							para la flexibilidad máxima del diseño.</p>
 				</div>
 			</div>
 
@@ -122,9 +119,21 @@
 				<div class="meta">
 					<h3 class="meta__title">COLONIAL MEXICANO</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal4">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal4" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>Colonial Mexicano</h2>
+					<img src="images/Exterior/COLONIAL MEXICANO.jpg" />
+					<P>deal para vialidades, estacionamientos, jardines, parques, áreas recreativas. Luminario exterior decorativo,</br>
+						 de alto rendimiento. Cápsula en fundición de aluminio bajo en cobre. Fabricada con un tratamiento previo para</br>
+						  una mayor resistencia a la corrosión. Balastro electromagnético autorregulado con un alto factor de potencia.</br>
+							 Refractor prismático de cristal Endural de bajo brillo con curva de distribución III. Fácil instalación y mantenimient</p>
+				</div>
 			</div>
 
 			<div class="grid__item grid__item--size-a shirts">
@@ -136,9 +145,21 @@
 				<div class="meta">
 					<h3 class="meta__title">2GT8</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal5">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal5" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>2GT8</h2>
+					<img src="images/Fluorescente/2GT8.jpg" />
+					<P>El primer luminario con lente de acrílico para empotrar, diseñado exclusivamente para lamparas T8,</br>
+						 balastros electrónicos T8 de bajo perfil y portalámparas compactas. De alta eficiencia, fácil y </br>
+						 rápido mantenimiento, construcción robusta y calidad asegurada. Compatible con casi todos los tipos</br>
+						  de plafón, ideal para cualquier instalación comercial.</p>
+				</div>
 			</div>
 
 			<div class="grid__item trousers">
@@ -150,9 +171,20 @@
 				<div class="meta">
 					<h3 class="meta__title">ELM-ELM2</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal6">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal6" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>ELM-ELM2</h2>
+					<img src="images/EMERGENCIA/ELM-ELM2.jpg" />
+					<P>Unidades de emergencia, con baterías de plomo-calcio libres de mantenimiento.</br>
+						 Carcasa de policarbonato, resistente a impactos, corrosión y a la decoloración causada</br>
+						  por los rayos UV.  Bajo consumo de energía gracias al uso de LED como fuente de luz.</p>
+				</div>
 			</div>
 
 			<div class="grid__item grid__item--size-a jackets">
@@ -164,10 +196,25 @@
 				<div class="meta">
 					<h3 class="meta__title">BANTAM 2000 PRISMALUNE</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal7">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
 			</div>
+
+			<div id="modal7" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>BANTAM 2000 PRISMALUNE</h2>
+					<img src="images/INDUSTRIAL/BANTAM 2000 PRISMALUNE.jpg" />
+					<P>Ideal para áreas de manufactura y ensamble, hangares, bodegas. Reflector de cristal prismático,</br>
+						 abierto o con lente de cristal termotemplado. Cápsula de balastro en diseño de bajo perfil para</br>
+						  uso en aplicaciones de espacio limitado y techos de baja altura. Fabricada con un tratamiento</br>
+							 previo para mayor resistencia a la corrosión. Balastro autorregulado de alto factor de potencia</br>
+							  con embobinados de cobre para dar la emisión lumínica completa. </p>
+				</div>
+			</div>
+
+
 
 			<div class="grid__item shoes">
 				<div class="slider">
@@ -178,9 +225,25 @@
 				<div class="meta">
 					<h3 class="meta__title">CIS-ILUMINARE (GRANDE Y PEQUEÑO)</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal8">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal8" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>CIS-ILUMINARE (GRANDE Y PEQUEÑO)</h2>
+					<img src="images/INTERIOR/CIS-ILUMINARE (GRANDE Y PEQUEÑO).jpg" />
+					<P>ideal para el comercios minoristas, aeropuertos, vestíbulos, escuelas. La línea Illuminaire</br>
+						incluye cinco nuevos estilos atractivos: el huevo, Crest, Tri huevo-, tri-Cresta y el cuádruple.</br>
+						Cada nueva forma (excepto la Quad) está disponible en un tamaño grande o compacto para dar cabida</br>
+						a una gama de alturas de montaje. Todos los accesorios se ofrecen con una variedad de distribuciones</br>
+						fotométricas (11 en la versión grande y cuatro en la versión compacta) para colocar de manera</br>
+						eficiente la luz donde se necesita. Las luminarias también hasta el 92 por ciento de eficiencia,</br>
+						que se traduce en una mayor uniformidad de iluminación con menos accesorios. reflectores de vidrio</br>
+						borosilicato resistente / refractores garantizar un rendimiento confiable, a largo plazo. </p>
+				</div>
 			</div>
 
 			<div class="grid__item shirts">
@@ -192,10 +255,23 @@
 				<div class="meta">
 					<h3 class="meta__title">2PMN3</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href=#modal9>Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
 			</div>
+
+			<div id="modal9" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>2PMN3</h2>
+					<img src="images/Fluorescente/2PM3N.jpg" />
+					<P>Luminario parabólico ideal para aplicaciones comerciales y en oficinas. Su diseño </br>
+						facilita el desempeño optimo de las lamparas T8 y balastros electrónicos. Las rejillas</br>
+						 forman una parábola exacta para obtener máxima eficiencia, excelente control óptico y </br>
+						 confort visual. </p>
+				</div>
+			</div>
+
 
 			<div class="grid__item shoes">
 				<div class="slider">
@@ -206,9 +282,24 @@
 				<div class="meta">
 					<h3 class="meta__title">illumibay</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal10">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal10" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>illumibay</h2>
+					<img src="images/INTERIOR/illumibay.jpg" />
+					<P>Para las tiendas de comestibles, librerias, Oficinas, Centros de Recreación, Escuelas.</br>
+						 Un diseño agradable, cuya mitad superior está esculpida, hace que la mayor parte de la </br>
+						 belleza inherente a nuestra vidrio. El versátil Illumibay es visualmente atractivo y cuenta</br>
+						  con la revolucionaria avanzada ISD (Distribución Ideal sinérgica) óptica en supercristal.</br>
+							 Está disponible en una variedad de fuentes de luz como lámparas fluorescentes compactas,</br>
+							 metalicos cerámica. Holophane ha creado un nuevo estándar industrial para la iluminación </br>
+							 de gran altura.</p>
+				</div>
 			</div>
 
 			<div class="grid__item dresses">
@@ -220,9 +311,22 @@
 				<div class="meta">
 					<h3 class="meta__title">DORCHESTER</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal11">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal11" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>DORCHESTER</h2>
+					<img src="images/EXTERIOR/DORCHESTER.jpg" />
+					<P>Especial para las calles, estacionamientos, escuelas, desarrollos comerciales, plazas, barrios</br>
+						históricos, plazas de pueblos, zonas residenciales, parques, paseos, bulevares, caminos. Diseño basado</br>
+						en las farolas las zonas urbanas de finales del siglo XIX. Este fue un momento en estilo victoriano y </br>
+						la elegancia fueron comparados con el suave resplandor de las lámparas de gas. Hoy en día, esta luminaria</br>
+						ofrece un estilo auténtico, con fuentes de luz moderna y componentes eléctricos. </p>
+				</div>
 			</div>
 
 			<div class="grid__item shirts">
@@ -234,9 +338,22 @@
 				<div class="meta">
 					<h3 class="meta__title">ATLANTA</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal12">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal12" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>ATLANTA</h2>
+					<img src="images/Fluorescente/ATLANTA.jpg"/>
+					<P>Ideal para estacionamientos cubiertos, pasillos, áreas de cargas, recepciones, áreas de producción</br>
+						 o proceso. Luminario de estilo contemporáneo con controlente refractivo prismático. Cuerpo en lámina</br>
+						  de aluminio rechazado. Fabricado con un tratamiento previo para una mayor resistencia a la corrosión.</br>
+							 Balastro electrónico de encendido rápido, bajo consumo de energía y alto factor de potencia, con rango</br>
+							  de operacion de 127 a 277V. Controlente de resina acrílica con reflector anodizado de alta reflectancia. Fácil instalación.</p>
+				</div>
 			</div>
 
 			<div class="grid__item trousers">
@@ -248,9 +365,24 @@
 				<div class="meta">
 					<h3 class="meta__title">ind</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal13">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
+			</div>
+
+			<div id="modal13" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>ind</h2>
+					<img src="images/EMERGENCIA/ind.jpg"/>
+					<P>Proporciona un mínimo de 90 minutos de la iluminación de la potencia nominal en caso de pérdida</br>
+						 de alimentación de CA. Único, de diseño innovador para una gran variedad de luz y usos industriales</br>
+						  pesados y ambientes comerciales. Superior rendimiento de la lámpara en mayores alturas de montaje.</br>
+							 Perfecto para el postes y columnas de montaje. Carcasa robusta, 0.140 "de espesor, moldeada por</br>
+							 inyección termoplástica. Orientación vertical diseñado especialmente para el poste o columna de </br>
+							 montaje, También es adecuado para muros, montaje I-Beam y montaje pendiente con el equipo de accesorios,</br>
+							  disponible en modelos seleccionados.</p>
+				</div>
 			</div>
 
 			<div class="grid__item jackets">
@@ -262,10 +394,26 @@
 				<div class="meta">
 					<h3 class="meta__title">BANTAM 2000 PRISMATITE</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal14">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
 			</div>
+
+			<div id="modal14" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>BANTAM 2000 PRISMATITE</h2>
+					<img src="images/INDUSTRIAL/BANTAM 2000 PRISMATITE.JPG"/>
+					<P>Ideal para túneles, andenes, plataformas de carga, estacionamientos cubiertos, áreas de</br>
+						 almacenaje y naves industriales. Luminario a prueba de polvo y humedad. Cápsula en fundición</br>
+						  de aluminio fabricada con un tratamiento previo para una mayor resistencia a la corrosión.</br>
+							 Balastro electromagnético autorregulado con un alto factor de potencia. Conjunto óptico de </br>
+							 cristal de borosilicato prismático Endural resistente al choque térmico. Dos curvas de distribución:</br>
+							  simétrica y asimétrica.</p>
+				</div>
+			</div>
+
+
 
 			<div class="grid__item shoes">
 				<div class="slider">
@@ -276,10 +424,25 @@
 				<div class="meta">
 					<h3 class="meta__title">LOBAY</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal15">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
 			</div>
+
+			<div id="modal15" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>LOBAY</h2>
+					<img src="images/INTERIOR/LOBAY.png"/>
+					<P>Ideal para supermercados, almacenes, bodegas, fábricas, auditorios. Cuerpo fabricado en</br>
+						 lámina de aluminio rechazado con acabado anodizado. Balastro electromagnético autorregulado</br>
+						  o electrónico de bajo consumo de energía. Cuatro refractores de cristal con la opción de cubierta</br>
+							 de aluminio rechazado para evitar la aportación de luz hacia arriba de los 180°. Cableado por la</br>
+							  parte superior del luminario, cuenta con entrata de 1/2" NPT para colgar el tubo conduit.</p>
+				</div>
+			</div>
+
+
 
 			<div class="grid__item grid__item--size-a dresses">
 				<div class="slider">
@@ -290,10 +453,25 @@
 				<div class="meta">
 					<h3 class="meta__title">GRAN VILLE</h3>
 					<span class="meta__brand">Artefactos Luminicos</span>
-					<span class="meta__price">Ver más</span>
+					<a class="meta__price" href="#modal16">Ver más</a>
 				</div>
 				<button class="action action--button action--buy"><i class="fa fa-shopping-cart"></i><span class="text-hidden">Add to cart</span></button>
 			</div>
+
+			<div id="modal16" class="modalmask">
+				<div class="modalbox rotate">
+					<a href="#close" title="Cerrar" class="close">X</a>
+					<h2>GRAN VILLE</h2>
+					<img src="images/EXTERIOR/GRAN VILLE.jpg"/>
+					<P>deal para jardines, camellones, muelles, estacionamientos, calles residenciales. </br>
+						Luminario exterior decorativo, de alto rendimiento. Cápsula en fundición de aluminio bajo en cobre.</br>
+						 Fabricada con un tratamiento previo para mayor resistencia a la corrosión. Balastro electromagnético</br>
+						  autorregulado con alto factor de potencia. Refractor y reflector prismáticos de cristal Endural de </br>
+							bajo brillo y alta reflectancia. Fácil instalación y mantenimiento.</p>
+				</div>
+			</div>
+
+
 
 			<div class="grid__item trousers">
 				<div class="slider">
