d17b6f12c9c097d3b3b89d60651f06d5a9c5f659
Nevin Santana <roll.93@hotmail.com>
2015-10-22 16:52:12 -0500

    pruebas con hotmail,gmail

--- Contacto2/send_form_email.php
+++ Contacto2/send_form_email.php
@@ -3,7 +3,7 @@ if(isset($_POST['email'])) {
 
     //
 
-    $email_to = "david@artefactosluminicos.com.mx";
+    $email_to = "digl_david23933@hotmail.com";
 
     $email_subject = "Contacto desde Web";
 
