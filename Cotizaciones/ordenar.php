b0a9eb9de1ba8ae0449999afe08b3ad7f009d587
Nevin Orlando Santana Escoto <roll.93@hotmail.com>
2016-02-24 11:11:28 -0600

    P#$%$ matúsculas en DB :-1:

--- Cotizaciones/ordenar.php
+++ Cotizaciones/ordenar.php
@@ -3,15 +3,15 @@
   include("funciones_mysql.php");
   $con=conectar();
   $i=""; $or=""; $idPartida="";
-  $sql="UPDATE partidas SET no_partida='-999' WHERE id_cotizacion='$cot'";
+  $sql="UPDATE Partidas SET no_partida='-999' WHERE id_cotizacion='$cot'";
   $res=query($sql, $con);
   foreach($_GET as $cam => $val) {
     if($cam=='cotizacion'){}
     else {
     $i++;
     $or=$_POST['orden'.$i];
-    $sql="UPDATE partidas SET no_partida='".$or."' WHERE id_partida='$val' AND
-    id_cotizacion = '$cot'";
+    $sql="UPDATE Partidas SET no_partida='$or' WHERE id_partida='$val' AND
+    id_cotizacion='$cot'";
     $res=query($sql, $con);
     }
   }
