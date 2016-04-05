5e8e7a45d3bf5ea65b6f034c40cbf2a6f45c30fd
Nevin Orlando Santana Escoto <roll.93@hotmail.com>
2016-04-05 12:44:46 -0500

    errores en menu

--- consecutivoProyectos/php/menu.php
+++ consecutivoProyectos/php/menu.php
@@ -4,12 +4,16 @@
     if($seccion=="principal") require_once("principal.php");
     if($seccion=="login") require_once("login.php");
     if($seccion=="proyectos") require_once("proyectos.php");
+    if($seccion=="proyectos") require_once("botonesCrear.php");
     if($seccion=="clientes") require_once("clientes.php");
     if($seccion=="cotizaciones") require_once("cotizaciones.php");
     if($seccion=="verCotizacion") require_once("verCotizacion.php");
     if($seccion=="eliminarCotizacion") require_once("eliminarCotizacion.php");
-    if($seccion=="proyectos") require_once("botonesCrear.php");
     if($seccion=="gestionProyecto") require_once("gestionProyecto.php");
-    else header("location: index.php");
+    if($seccion!="principal" && $seccion!="login" && $seccion!="proyectos"
+    && $seccion!="clientes" && $seccion!="cotizaciones" &&
+    $seccion!="verCotizacion" && $seccion!="eliminarCotizacion" &&
+    $seccion!="eliminarCotizacion" && $seccion!="gestionProyecto")
+      header("location: index.php");
   }
 ?>
