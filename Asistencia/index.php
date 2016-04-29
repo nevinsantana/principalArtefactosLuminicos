01d99dc1f32a12c9e8ed28b7ffe46e1e3ed98f18
Nevin Orlando Santana Escoto <roll.93@hotmail.com>
2016-04-29 12:03:49 -0500

    op

--- Asistencia/index.php
+++ Asistencia/index.php
@@ -10,6 +10,10 @@
     <link rel='icon' type='image/png' href='img/favicon.png' sizes='64x64'>
     <link rel="stylesheet" href="../resources/fAwesome/css/font-awesome.min.css">
     <link rel='stylesheet' href='css/style.css'>
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+    <link rel="stylesheet" type="text/css" href="css/content.css" />
+    <script src="js/modernizr.custom.js"></script>
   	<title>Asistencia</title>
     <script src='../js/jquery.js'></script>
   </head>
@@ -23,16 +27,84 @@
   	<main id='persTUsersContainer'>
       <section id='persContainer'>
         <div class='rDivision'>
-          <input type='button' value='Quincena anterior' onclick='aTabla1(y)'
-          id='b1'>
+          <div><input type='button' value='Quincena anterior' onclick='aTabla1(y)'
+          id='b1'></div>
+        </div>
+        <div class="morph-button morph-button-inflow morph-button-inflow-1">
+          <button type="button"><span>:v</span></button>
+          <div class="morph-content">
+            <div>
+              <div class="content-style-form content-style-form-4">
+                <h2 class="morph-clone">Quincena anterior</h2>
+                <form>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <table>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                  </table>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="morph-button morph-button-inflow morph-button-inflow-1">
+          <button type="button"><span>:v</span></button>
+          <div class="morph-content">
+            <div>
+              <div class="content-style-form content-style-form-4">
+                <h2 class="morph-clone">Mes anterior</h2>
+                <form>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <table>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                  </table>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="morph-button morph-button-inflow morph-button-inflow-1">
+          <button type="button"><span>:v</span></button>
+          <div class="morph-content">
+            <div>
+              <div class="content-style-form content-style-form-4">
+                <h2 class="morph-clone">Personalizar</h2>
+                <form>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <p><label>Your Email Address</label><input type="text"/><span>We promise, we won't send you any spam. Just love.</span></p>
+                  <table>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td></tr>
+                  </table>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
         <div class='rDivision'>
-          <input type='button' value='Mes anterior' onclick='aTabla2(y)'
-          id='b2'>
+          <div><input type='button' value='Mes anterior' onclick='aTabla2(y)'
+          id='b2'></div>
         </div>
         <div class='rDivision'>
-          <input type='button' value='Personalizar' onclick='aTabla3(y)'
-          id='b3'>
+          <div><input type='button' value='Personalizar' onclick='aTabla3(y)'
+          id='b3'></div>
         </div>
       </section>
       <section id='tUsersContainer'>
@@ -59,5 +131,13 @@
     </main>
     <script>var y=<?php echo $y; ?></script>
     <script src='js/functions.js'></script>
+    <script src="js/classie.js"></script>
+		<script src="js/uiMorphingButton_inflow.js"></script>
+    <script>
+      [].slice.call(document.querySelectorAll('.morph-button')).forEach(function(bttn) {
+        new UIMorphingButton( bttn, {
+        } );
+      } );
+    </script>
   </body>
 </html>
