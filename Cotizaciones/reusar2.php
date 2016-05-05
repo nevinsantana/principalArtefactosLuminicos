74615f71ac290fe2ab94e3c7cc6afc7cfca0c0c2
Nevin Orlando Santana Escoto <roll.93@hotmail.com>
2016-05-05 11:39:22 -0500

    comentarios reusar

--- Cotizaciones/reusar2.php
+++ Cotizaciones/reusar2.php
@@ -7,10 +7,10 @@
   $id_cotizacion2=$_GET['id_cotizacion2'];
   $empresa=$_POST['empresa'];
   $sql="SELECT * FROM Clientes WHERE empresa='$empresa'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res); $id_num_cliente=$cam['id_num_cliente'];
   $sql="SELECT * FROM Cotizaciones WHERE id_cotizacion='$id_cotizacion'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res);
   foreach ($cam as $camp => $value) { ${$camp}=$value; }
   $sql="INSERT INTO Cotizaciones(id_cotizacion, fecha, id_cliente, id_usuario,
@@ -18,24 +18,24 @@
     descuento, activo) VALUES('$id_cotizacion2','$fecha','$id_num_cliente',
     '$id_usuario','$vigencia','$no_partidas','$divisa','$subtotal','$iva',
     '$total','$t_entrega','$c_pago','$descuento','$activo')";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $sql="SELECT * FROM Clientes WHERE empresa='$empresa'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res);
   $id_direccion=$cam['id_direccion'];
   $sql="SELECT * FROM Direcciones WHERE id_direccion=$id_direccion";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res);
   foreach ($cam as $camp => $value) { ${$camp}=$value; }
   $sql="SELECT * FROM Clientes WHERE empresa='$empresa'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res); $id_contacto=$cam['id_contacto'];
   $sql="SELECT * FROM Contacto WHERE id_contacto='$id_contacto'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $cam=mysql_fetch_assoc($res);
   foreach ($cam as $camp => $value) { ${$camp}=$value; }
   $sql="SELECT * FROM Usuarios WHERE id_usuario='$id_usuario'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $usuario=mysql_fetch_assoc($res);
   $nombre=$usuario['nombre'];
   $apellido_p=$usuario['apellido_p'];
@@ -48,9 +48,9 @@
   $sql="INSERT INTO Datos_Cotizacion(id_cotizacion, datos_cliente,
     datos_contacto, datos_vendedor) VALUES('$id_cotizacion2','$datos_cliente',
     '$datos_contacto','$datos_vendedor')";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   $sql="SELECT * FROM Partidas WHERE id_cotizacion='$id_cotizacion'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   while($cam=mysql_fetch_assoc($res)) {
     $id_partida=$cam['id_partida'];
     foreach ($cam as $camp => $value) { ${$camp}=$value; }
@@ -61,12 +61,12 @@
     $res1=query($sql, $con);
   }
   $sql="SELECT * FROM Notas WHERE id_cotizacion='$id_cotizacion'";
-  //$res=query($sql, $con);
+  $res=query($sql, $con);
   while($cam=mysql_fetch_assoc($res)) {
     $no_nota=$cam['no_nota']; $descripcion_nota=$cam['descripcion'];
     $sql="INSERT INTO Notas(id_cotizacion, no_nota, descripcion)
       VALUES('$id_cotizacion2','$no_nota','$descripcion_nota')";
     $res1=query($sql, $con);
   }
-  //header("Location: editar_cotizacion.php?id_cotizacion=$id_cotizacion2");
+  header("Location: editar_cotizacion.php?id_cotizacion=$id_cotizacion2");
 ?>
